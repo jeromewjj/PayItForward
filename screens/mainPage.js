41205228fb0e9f9c20c7ff5a8434845dc8b0fe21
yl-ang <yongliangang@gmail.com>
2022-07-10 09:44:28 +0800

    Add main page simple logic

--- screens/mainPage.js
+++ screens/mainPage.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import { Layout, Text } from '@ui-kitten/components';
+import { Button } from '@ui-kitten/components';
+import { useNavigation } from "@react-navigation/native";
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 export default MainPage = () => {
+
+    const navigation = useNavigation();
+
+    const redirectToDonationPage = () => {
+        navigation.navigate("Donation");
+    }
+
     return (
-        <Text category='h1'>Main Page!</Text>
+        <View style={styles.container}>
+            <Text category='h1'>Welcome Back!</Text>
+            <Button style={styles.button} onPress={
+                () =>  Alert.alert("Logout?",
+                        "",
+                        [
+                            {
+                                text: "QR (Camera)",
+                                onPress: () => console.log("Open camera app"),
+                            },
+                            {
+                                text: "NFC",
+                                onPress: () => console.log("NFC"),
+                            },
+                        ])}>PAY</Button>
+            <Button style={styles.button} onPress={() => redirectToDonationPage()}>DONATE</Button>
+        </View>
     );
-  }
+}
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 8,
+        justifyContent: 'center',
+        backgroundColor: '#ED1A3B',
+        textAlign: "center",
+        alignSelf:"center",
+        margin: 20,
+        width: 200,
+        height: 100,
+    }
+})
